f4d482a3e749448f3f16a3d859859f0483263d2f
andyrm8052 <60273802+andyrm8052@users.noreply.github.com>
2020-12-12 21:21:12 -0500

    signuptest js
    code modifications

--- docs/js/signuptest.js
+++ docs/js/signuptest.js
@@ -18,12 +18,10 @@ function textReg(TValue,TError){
     if (!/\d/.test(getValue(TValue))) {
         setValue(TError,"");
         //clear error message when text follow reg expression
-		return true;
     }
     else {
         setValue(TError,valueError);
         //set value error when text does not follow reg expression
-		return valueError;
     }
 }
 
@@ -33,12 +31,10 @@ function emailReg(ERValue,ERError){
     if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(getValue(ERValue))) {
         setValue(ERError,"");
         //clear error message when email follow reg expression
-		return true;
     }
     else {
         setValue(ERError,valueError);
         //set value error when email does not follow reg expression
-		return valueError;
     }
 }
 
@@ -48,12 +44,10 @@ function passReg(PRValue,PRError){
     if (/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{2,20}$/.test(getValue(PRValue))) {
         setValue(PRError,"");
         //clear error message when email follow reg expression
-		return true;
     }
     else {
         setValue(PRError,valueError);
         //set value error when email does not follow reg expression
-		return valueError;
     }
 }
 
@@ -63,14 +57,10 @@ function textChecker(TCValue, TCError){
     if(getValue(TCValue)) {
         textReg(TCValue,TCError);
         //check text using reg expression
-		if (textReg != "" || textReg != NULL) {
-			return true;
-		}
     }
     else {
         setValue(TCError,errors);
         //sets empty error when text is empty
-			return error
     }
 }
 
@@ -80,13 +70,9 @@ function emailChecker(ECValue, ECError) {
     if (getValue(ECValue)) {
         emailReg(ECValue, ECError);
         //check email using reg expression
-		if (emailReg != "" || passReg != NULL){
-			return true;
-		}
     } else {
         setValue(ECError, errors);
         //sets empty error when email is empty
-			return error;
     }
 }
 
@@ -96,13 +82,9 @@ function passChecker(PCValue, PCError) {
     if (getValue(PCValue)) {
         passReg(PCValue, PCError);
         //check password using reg expression
-		if (passReg != "" || passReg != NULL){
-			return true;
-		}
     } else {
         setValue(PCError, errors);
         //sets empty error when password is empty
-			return error;
     }
 }
 
